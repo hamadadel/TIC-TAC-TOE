4c960bac2cf54eb8d1d3d5bdae5f471a845783a7
Hamad Adel <hamadwebdeveloper@gmail.com>
2024-06-23 13:12:00 +0300

    create BaseCell component

---
+++ src/components/BaseCell.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="cell" @click="shot">
+    {{ value }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCell',
+  props: ['marker', 'winner', 'value'],
+  emits: ['shot'],
+  data() {
+    return {
+      canMark: true,
+    };
+  },
+  methods: {
+    shot() {
+      if (!this.winner && this.canMark) {
+        // this.value = this.$parent.activePlayer;
+        this.canMark = false;
+        this.$emit('shot', this.marker);
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+.cell {
+  height: 120px;
+  line-height: 120px;
+  font-size: 3.5em;
+  background: #2c3e50;
+}
+.cell:hover {
+  background: #405971;
+  cursor: pointer;
+}
+</style>
