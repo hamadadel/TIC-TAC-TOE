6020bb8ab125b0d7da69544b79f41ec42cb4d1ad
Hamad Adel <hamadwebdeveloper@gmail.com>
2024-06-23 13:12:35 +0300

    create RestartButton component

---
+++ src/components/RestartButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="restart">Restart</button>
+</template>
+<script>
+export default {
+  name: 'RestartButton',
+};
+</script>
+<style>
+.restart {
+  width: 100%;
+  margin: 0;
+  background: #ff5722;
+  color: white;
+  border: none;
+  padding: 20px 0px;
+  border-bottom-left-radius: 50px;
+  border-bottom-right-radius: 50px;
+  font-weight: bold;
+  cursor: pointer;
+  outline: none;
+  font-size: 2em;
+}
+</style>
