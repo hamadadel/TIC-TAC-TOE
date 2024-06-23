5037d128022eebbecc555fb61a8a2b1ebc47aea8
Hamad Adel <hamadwebdeveloper@gmail.com>
2024-06-23 12:54:55 +0300

    split the game into multiple components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-header />
+  <base-grid />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from '@/components/TheHeader.vue';
+import BaseGrid from '@/components/BaseGrid.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TheHeader,
+    BaseGrid,
+  },
+  data() {
+    return {
+      matches: 0,
+      wins: {
+        O: 0,
+        X: 0,
+      },
+    };
+  },
+};
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  max-width: 400px;
 }
 </style>
