b7e4087e4f3041e6d139b3309edd8033acba765c
Hamad Adel <hamadwebdeveloper@gmail.com>
2024-06-23 12:55:40 +0300

    create TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Tic Tac Toe</h1>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+};
+</script>
+<style>
+h1 {
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 3em;
+}
+</style>
